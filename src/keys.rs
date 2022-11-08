3f73d1619a3f2d27e72713aaf25915c9018191c5
Archer Fabling <archie4575@gmail.com>
2022-11-08 16:13:27 +0800

    alkdjaw

--- src/keys.rs
+++ src/keys.rs
@@ -1,3 +1,8 @@
+/* 
+    Key Pair Usage
+*/
+
+
 pub struct Key {
     pub n: u64,
     pub exp: u64,
@@ -6,15 +11,11 @@ pub struct Key {
 impl Key {
     pub fn encrypt(&self, t: u64) -> u64 {
         let exp_table:[u64; 64] = self.gen_table(t);
-        for i in 0..64 {
-            println!("table[{}] = {}", i, exp_table[i]);
-        }
         let mut acc: u64 = 1;
         let mut idx: u64 = self.exp;
         for i in 0..64 {
             if (idx & 1) == 1 {
                 acc = (acc * exp_table[i]) % self.n;
-                println!("idx: {} exp_table[{}]: {}", idx, i, exp_table[i]);
             }
             idx = idx >> 1;
         }
@@ -39,7 +40,48 @@ pub struct KeyPair {
 
 impl KeyPair {
     fn new () -> Self {
-        KeyPair { skey: }
+    let p: u64 = 61;
+    let q: u64 = 53;
+    let n: u64 = q * p;
+    let m: u64 = (p - 1) * (q - 1);
+    let e: u64 = findE(m);
+    let d: u64 = (1 + n * m) / e;
+
+    println!("{:?}", (d, e, m, n));
+    }
+
+    fn RandExponents() {
+
+    }
+
+    fn Gcd(first: usize, second: usize) -> usize {
+        let mut max = first;
+        let mut min = second;
+        if min > max {
+            let val = max;
+            max = min;
+            min = val;
+        }
+    
+        loop {
+            let res = max % min;
+            if res == 0 {
+                return min;
+            }
+    
+            max = min;
+            min = res;
+        }
+    }
+    
+    fn findE(m: u64) -> u64 {
+        // let e: Vec<u64> = range(2, m);
+        for n in 2..m {
+            if Gcd(n, m) == 1 {
+                return n;
+            }
+        }
+        return 1;
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,20 @@ pub mod keys;
 fn main() {
     let pair: KeyPair = keyGen::Generate();
 
+    
     let t: u64 = 855;
+    test(t);
+
+    let n: u32 = 121;
+    test(t);
+    
     let c: u64 = pair.pkey.encrypt(t);
     println!("{}", c);
 
 
+}
+
+fn test(n: usize) {
+    prinln!("{}", size_of(usize));
+    prinln!("{}", size_of(n));
 }
