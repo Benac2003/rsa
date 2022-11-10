dd7af5a030ef99e69f82fa12e3619f86b7bd54c0
Benac2003 <22653713+Benac2003@users.noreply.github.com>
2022-11-10 14:14:38 +0800

    Finished Random Key Generator

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
-use crate::keys::{KeyPairGenerate, KeyPair, Key};
+use crate::keys::{KeyPair};
 pub mod keys;
 
 
 fn main() {
-    let pair: KeyPair = KeyPairGenerate::new();
-    pair.calcExponents();
-    
+    let pair: KeyPair = KeyPair::new().Generate();
     let t: u64 = 855;
   //  test(t);
 
     let n: u32 = 121;
 //    test(t);
     
-    let c: u64 = pair.pkey.encrypt(t);
-    println!("{}", c);
+    //let c: u64 = pair.pkey.encrypt64(t);
+  //  println!("{}", c);
 
 
 }
